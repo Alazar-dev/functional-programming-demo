0d6fe822dc49c301d46248db0c1f396b11336e49
Alazar-dev <mealazartadesse@gmail.com>
2022-06-24 10:27:42 +0300

    edit todo

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let todos = []
+let todos = [
+    {
+        title: 'learn functional programming',
+        completed: false,
+        due_date: 'Jue 12',
+        published_at: "2022-06-23T13:28:22.026Z"
+    }
+]
 
 function addTodo(title, due_date) {
     return todos.push({title: title, completed: false, due_date: due_date, published_at: new Date(), })
 }
 
-addTodo("learn functional programming", 'Jue 12');
+function editTodo(index, title, due_date) {
+    let todo = todos[index];
+    todo.title = title;
+    todo.due_date = due_date;
+}
 
+editTodo(0, "edited title", "Nov 7");
